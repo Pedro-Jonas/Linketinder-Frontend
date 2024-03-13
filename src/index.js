47cfdb8de899b391d6332a8e1391cfcfacbd8782
Pedro Jonas <pedrojonas.pj77@gmail.com>
2024-03-13 09:09:05 -0300

    feat: mostar formulários

---
+++ src/index.js
@@ -0,0 +1,21 @@
+"use strict";
+const buttonSignUp = document.getElementById("buttonSignUp");
+const signUp = document.getElementById("signUp");
+const formCandidade = document.getElementById("formSignUpCandidate");
+const formCompanies = document.getElementById("formSignUpCompanies");
+function showOptionsSignUp() {
+    buttonSignUp === null || buttonSignUp === void 0 ? void 0 : buttonSignUp.classList.toggle("buttonActionClicked");
+    signUp === null || signUp === void 0 ? void 0 : signUp.classList.toggle("hide");
+}
+function showFormCandidade() {
+    if (!(formCompanies === null || formCompanies === void 0 ? void 0 : formCompanies.classList.contains("hide"))) {
+        formCompanies === null || formCompanies === void 0 ? void 0 : formCompanies.classList.add("hide");
+    }
+    formCandidade === null || formCandidade === void 0 ? void 0 : formCandidade.classList.remove("hide");
+}
+function showFormCompanies() {
+    if (!(formCandidade === null || formCandidade === void 0 ? void 0 : formCandidade.classList.contains("hide"))) {
+        formCandidade === null || formCandidade === void 0 ? void 0 : formCandidade.classList.add("hide");
+    }
+    formCompanies === null || formCompanies === void 0 ? void 0 : formCompanies.classList.remove("hide");
+}
---
+++ src/index.ts
@@ -0,0 +1,25 @@
+const buttonSignUp = document.getElementById("buttonSignUp")
+const signUp = document.getElementById("signUp")
+
+const formCandidade = document.getElementById("formSignUpCandidate")
+const formCompanies = document.getElementById("formSignUpCompanies")
+
+
+function showOptionsSignUp() : void {
+   buttonSignUp?.classList.toggle("buttonActionClicked")
+   signUp?.classList.toggle("hide")
+}
+
+function showFormCandidade() : void {
+   if(!formCompanies?.classList.contains("hide")){
+      formCompanies?.classList.add("hide")
+   }
+   formCandidade?.classList.remove("hide")
+}
+
+function showFormCompanies() : void {
+   if(!formCandidade?.classList.contains("hide")){
+      formCandidade?.classList.add("hide")
+   }
+   formCompanies?.classList.remove("hide")
+}
