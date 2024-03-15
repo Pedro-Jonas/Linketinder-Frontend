febb754d777069e8a912c15503fb32f2e548d9d0
Pedro Jonas <pedrojonas.pj77@gmail.com>
2024-03-14 23:17:58 -0300

    feat: mostrar visão de candidato

--- src/index.js
+++ src/index.js
@@ -1,23 +1,47 @@
 "use strict";
 const candidates = JSON.parse(localStorage.getItem("candidates") || '[]');
 const companies = JSON.parse(localStorage.getItem("companies") || '[]');
+const divToCandidate = document.querySelector(".showToCandidate");
+const divToCompanie = document.querySelector(".showToCompanie");
 const buttonSignUp = document.getElementById("buttonSignUp");
 const signUp = document.getElementById("signUp");
 const formCandidade = document.getElementById("formSignUpCandidate");
 const formCompanies = document.getElementById("formSignUpCompanies");
+let htmlToCandidate = ``;
+for (let companie of companies) {
+    htmlToCandidate += `
+   <div class="boxCompanie">
+      <p>Empresa ${companies.indexOf(companie) + 1}</p>
+      <p>estado: ${companie.state}</p>
+      <p>país: ${companie.country}</p>
+      <p>descrição: ${companie.descripition}</p>
+      <div id=${companies.indexOf(companie)}>
+         <div class="buttonShowJobs" onclick="showJobs(id)" id =${companies.indexOf(companie)}>
+            Ver vagas
+         </div>
+      </div>
+   </div>
+   `;
+}
 function showOptionsSignUp() {
     buttonSignUp === null || buttonSignUp === void 0 ? void 0 : buttonSignUp.classList.toggle("buttonActionClicked");
     signUp === null || signUp === void 0 ? void 0 : signUp.classList.toggle("hide");
 }
 function showFormCandidade() {
-    if (!(formCompanies === null || formCompanies === void 0 ? void 0 : formCompanies.classList.contains("hide"))) {
-        formCompanies === null || formCompanies === void 0 ? void 0 : formCompanies.classList.add("hide");
+    if (!formCompanies.classList.contains("hide")) {
+        formCompanies.classList.add("hide");
     }
-    formCandidade === null || formCandidade === void 0 ? void 0 : formCandidade.classList.remove("hide");
+    if (!divToCandidate.classList.contains("hide")) {
+        divToCandidate.classList.add("hide");
+    }
+    formCandidade.classList.remove("hide");
 }
 function showFormCompanies() {
     if (!(formCandidade === null || formCandidade === void 0 ? void 0 : formCandidade.classList.contains("hide"))) {
         formCandidade === null || formCandidade === void 0 ? void 0 : formCandidade.classList.add("hide");
+    }
+    if (!divToCandidate.classList.contains("hide")) {
+        divToCandidate.classList.add("hide");
     }
     formCompanies === null || formCompanies === void 0 ? void 0 : formCompanies.classList.remove("hide");
 }
@@ -33,10 +57,10 @@ function createNewCandidade() {
         state: document.getElementById("candidateState").value,
         cep: document.getElementById("candidateCep").value,
         descripition: document.getElementById("candidateDescripition").value,
-        skills: document.getElementById("candidateSkills").value,
+        skills: document.getElementById("candidateSkills").value.split(","),
     };
     candidates.push(newCanditade);
-    localStorage.setItem("candidates", JSON.stringify([candidates]));
+    localStorage.setItem("candidates", JSON.stringify(candidates));
 }
 function createNewCompanie() {
     const newCompanie = {
@@ -47,12 +71,29 @@ function createNewCompanie() {
         state: document.getElementById("companieState").value,
         cep: document.getElementById("companieCep").value,
         descripition: document.getElementById("companieDescripition").value,
+        jobVacancies: [],
     };
     companies.push(newCompanie);
-    localStorage.setItem("companies", JSON.stringify([companies]));
+    localStorage.setItem("companies", JSON.stringify(companies));
 }
+let clickedShowToCandidate = false;
 function showToCandidate() {
-    reload();
+    if (!clickedShowToCandidate) {
+        divToCandidate.innerHTML += htmlToCandidate;
+        divToCandidate.classList.remove("hide");
+        clickedShowToCandidate = true;
+    }
+}
+function showJobs(id) {
+    const searchCompanie = document.getElementById(id);
+    const idNumber = parseInt(id);
+    searchCompanie.innerHTML = `
+   <div>
+      ${companies[idNumber].jobVacancies ?
+        companies[idNumber].jobVacancies :
+        `<p>Não há vagas!</p>`}
+   </div>
+   `;
 }
 function showToCompanie() {
     reload();
--- src/index.ts
+++ src/index.ts
@@ -1,90 +1,151 @@
+
+type JobVacancies = {
+   title: string;
+   description: string;
+   skills: string[];
+}
+
 type candidate = {
    name: string;
    email: string;
-   cpf:string;
+   cpf: string;
    age: string;
    state: string;
    cep: string;
    descripition: string;
-   skills: string;
+   skills: string[];
 }
 
 type companie = {
    name: string;
    email: string;
-   cnpj:string;
+   cnpj: string;
    country: string;
    state: string;
    cep: string;
    descripition: string;
+   jobVacancies?: JobVacancies[] | [];
 }
 
-const candidates : candidate[] = JSON.parse(localStorage.getItem("candidates") || '[]');
-const companies : companie[] = JSON.parse(localStorage.getItem("companies") || '[]');
+const candidates: candidate[] = JSON.parse(localStorage.getItem("candidates") || '[]');
+const companies: companie[] = JSON.parse(localStorage.getItem("companies") || '[]');
+
+const divToCandidate = document.querySelector(".showToCandidate") as HTMLBodyElement;
+const divToCompanie = document.querySelector(".showToCompanie") as HTMLBodyElement;
+
 
 const buttonSignUp = document.getElementById("buttonSignUp");
 const signUp = document.getElementById("signUp");
 
-const formCandidade = document.getElementById("formSignUpCandidate");
-const formCompanies = document.getElementById("formSignUpCompanies");
+const formCandidade = (document.getElementById("formSignUpCandidate") as HTMLBodyElement);
+const formCompanies = (document.getElementById("formSignUpCompanies") as HTMLBodyElement);
+
+let htmlToCandidate: string = ``;
 
 
-function showOptionsSignUp() : void {
+for (let companie of companies) {
+
+   htmlToCandidate += `
+   <div class="boxCompanie">
+      <p>Empresa ${companies.indexOf(companie) + 1}</p>
+      <p>estado: ${companie.state}</p>
+      <p>país: ${companie.country}</p>
+      <p>descrição: ${companie.descripition}</p>
+      <div id=${companies.indexOf(companie)}>
+         <div class="buttonShowJobs" onclick="showJobs(id)" id =${companies.indexOf(companie)}>
+            Ver vagas
+         </div>
+      </div>
+   </div>
+   `
+}
+
+function showOptionsSignUp(): void {
    buttonSignUp?.classList.toggle("buttonActionClicked")
    signUp?.classList.toggle("hide")
 }
 
-function showFormCandidade() : void {
-   if(!formCompanies?.classList.contains("hide")){
-      formCompanies?.classList.add("hide")
+function showFormCandidade(): void {
+   if (!formCompanies.classList.contains("hide")) {
+      formCompanies.classList.add("hide")
    }
-   formCandidade?.classList.remove("hide")
+   if (!divToCandidate.classList.contains("hide")) {
+      divToCandidate.classList.add("hide")
+   }
+   formCandidade.classList.remove("hide")
+   
 }
 
-function showFormCompanies() : void {
-   if(!formCandidade?.classList.contains("hide")){
+function showFormCompanies(): void {
+   if (!formCandidade?.classList.contains("hide")) {
       formCandidade?.classList.add("hide")
+   }
+   if (!divToCandidate.classList.contains("hide")) {
+      divToCandidate.classList.add("hide")
    }
    formCompanies?.classList.remove("hide")
 }
 
-function reload() : void {
+function reload(): void {
    window.location.reload()
 }
 
-function createNewCandidade() : void {
-   const newCanditade : candidate = {
+function createNewCandidade(): void {
+
+   const newCanditade: candidate = {
       name: (document.getElementById("candidateName") as HTMLFormElement).value,
       email: (document.getElementById("candidateEmail") as HTMLFormElement).value,
       cpf: (document.getElementById("candidateCpf") as HTMLFormElement).value,
       age: (document.getElementById("candidateAge") as HTMLFormElement).value,
       state: (document.getElementById("candidateState") as HTMLFormElement).value,
-      cep: (document.getElementById("candidateCep")as HTMLFormElement).value,
+      cep: (document.getElementById("candidateCep") as HTMLFormElement).value,
       descripition: (document.getElementById("candidateDescripition") as HTMLFormElement).value,
-      skills: (document.getElementById("candidateSkills") as HTMLFormElement).value,
+      skills: (document.getElementById("candidateSkills") as HTMLFormElement).value.split(","),
    }
+
    candidates.push(newCanditade)
-   localStorage.setItem("candidates", JSON.stringify([candidates]))
+   localStorage.setItem("candidates", JSON.stringify(candidates))
 }
 
-function createNewCompanie() : void {
-   const newCompanie : companie = {
+function createNewCompanie(): void {
+
+   const newCompanie: companie = {
       name: (document.getElementById("companieName") as HTMLFormElement).value,
       email: (document.getElementById("companieEmail") as HTMLFormElement).value,
       cnpj: (document.getElementById("companieCnpj") as HTMLFormElement).value,
       country: (document.getElementById("companieCountry") as HTMLFormElement).value,
       state: (document.getElementById("companieState") as HTMLFormElement).value,
-      cep: (document.getElementById("companieCep")as HTMLFormElement).value,
+      cep: (document.getElementById("companieCep") as HTMLFormElement).value,
       descripition: (document.getElementById("companieDescripition") as HTMLFormElement).value,
+      jobVacancies: [],
    }
+
    companies.push(newCompanie)
-   localStorage.setItem("companies", JSON.stringify([companies]))
+   localStorage.setItem("companies", JSON.stringify(companies))
 }
 
-function showToCandidate() : void {
-   reload()
+let clickedShowToCandidate = false;
+function showToCandidate(): void {
+   if (!clickedShowToCandidate) {
+      divToCandidate.innerHTML += htmlToCandidate;
+      divToCandidate.classList.remove("hide");
+      clickedShowToCandidate = true;
+   }
 }
 
-function showToCompanie() : void {
+function showJobs(id: string) {
+   const searchCompanie = (document.getElementById(id) as HTMLBodyElement);
+   const idNumber = parseInt(id);
+   searchCompanie.innerHTML = `
+   <div>
+      ${companies[idNumber].jobVacancies ? 
+      companies[idNumber].jobVacancies :
+      `<p>Não há vagas!</p>`
+      }
+   </div>
+   `
+}
+
+function showToCompanie(): void {
    reload()
 }
